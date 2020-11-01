5f650641696e8089bd599aba54779b79c03f04f9
cjdunteman <cdunteman@wisc.edu>
2020-11-01 13:11:54 -0600

    updated linux installs

--- install.sh
+++ install.sh
@@ -7,4 +7,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	source linux/linux_setup.sh
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	source mac/mac_setup.sh
+else
+    echo "Unable to detect operating system."
 fi
--- linux/linux_setup.sh
+++ linux/linux_setup.sh
@@ -4,6 +4,10 @@ sudo apt install git
 sudo apt install nodejs
 sudo apt install tree
 sudo apt install curl
+sudo apt install wget
+
+# java
+sudo apt install openjdk-11-jdk
 
 # rust lang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -18,5 +22,12 @@ sudo apt install python3-pip
 pip3 install --user --upgrade pip
 pip3 install --user --upgrade jupyter
 pip3 install --user --upgrade ipython-sql
+
+# applications
+sudo snap install discord
+sudo snap install --classic code
+wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+sudo apt install ./slack-desktop-*.deb
+sudo snap install spotify
 
 source symlinks.sh
