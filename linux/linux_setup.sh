9ee98ac580598efa84888f64eebee1f882d6383b
cjdunteman <cdunteman@wisc.edu>
2020-11-01 12:37:32 -0600

    added some packages

--- linux/linux_setup.sh
+++ linux/linux_setup.sh
@@ -3,6 +3,15 @@ sudo apt install xclip
 sudo apt install git
 sudo apt install nodejs
 sudo apt install tree
+sudo apt install curl
+
+# rust lang
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# web dev
+sudo apt install nodejs
+sudo apt install npm
+npm install -g gatsby-cli
 
 # python packages
 sudo apt install python3-pip
