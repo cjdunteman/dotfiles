eb9a9a845addab41c3af91e65363d3b5aef0f8cf
cjdunteman <cdunteman@wisc.edu>
2020-11-10 23:43:13 -0600

    added docker

--- linux/linux_setup.sh
+++ linux/linux_setup.sh
@@ -25,6 +25,7 @@ pip3 install --user --upgrade ipython-sql
 
 # applications
 sudo snap install discord
+sudo snap install docker
 sudo snap install --classic code
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
 sudo apt install ./slack-desktop-*.deb
