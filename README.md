4322cdda53eeba7688d86fb08133f956ae198b8e
cjdunteman <cjdunteman@gmail.com>
2021-05-29 09:44:44 -0500

    Pass tests

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 ![main](https://github.com/cjdunteman/dotfiles/workflows/main/badge.svg)
 
-## Dotfiles
+# Dotfiles
 
-These are my dotfiles for Linux and MacOS.
+These are my dotfiles for Ubuntu and MacOS.
 
+## Install
 
-### Setting Up a New Device
+Ubuntu:
 
-##### GitHub
+`bash -c "$(wget https://raw.githubusercontent.com/cjdunteman/dotfiles/master/ubuntu/install.sh)`
+
+#### Git
+
+Set git email and username
+
+`git config --global user.email "email@example.com"`
+
+`git config --global user.name "username"`
+
+#### GitHub
 
 Connect to GitHub with SSH following [these instructions](https://docs.github.com/en/github/authenticating-to-github/connecting-to-github-with-ssh).
 
 Manage commit signature following [these instructions](https://docs.github.com/en/github/authenticating-to-github/managing-commit-signature-verification).
 
+Sign all commits by default
+
+`git config --global commit.gpgsign true`
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,4 @@
+printf "Running Ubuntu install script."
 sudo apt install vim
 sudo apt install xclip
 sudo apt install git
@@ -5,9 +6,10 @@ sudo apt install nodejs
 sudo apt install tree
 sudo apt install curl
 sudo apt install wget
+sudo apt install gnupg
 
 # java
-sudo apt install openjdk-11-jdk
+# sudo apt install openjdk-11-jdk
 
 # rust lang
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
@@ -18,17 +20,30 @@ sudo apt install npm
 npm install -g gatsby-cli
 
 # python packages
-sudo apt install python3-pip
-pip3 install --user --upgrade pip
-pip3 install --user --upgrade jupyter
-pip3 install --user --upgrade ipython-sql
+# sudo apt install python3-pip
+#pip3 install --user --upgrade pip
+#pip3 install --user --upgrade jupyter
+#pip3 install --user --upgrade ipython-sql
+
+# docker
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo \ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+#sudo apt-get update
+#sudo apt-get install docker-ce docker-ce-cli containerd.io
+#sudo docker run hello-world
 
 # applications
 sudo snap install discord
-sudo snap install docker
-sudo snap install --classic code
-wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
-sudo apt install ./slack-desktop-*.deb
+#sudo snap install --classic code
+#wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.0.2-amd64.deb
+#sudo apt install ./slack-desktop-*.deb
 sudo snap install spotify
+#sudo snap install bitwarden
 
-source symlinks.sh
+# other
+sudo snap install htop
+
+# configurations
+#git config --global core.editor "vim"
+
+#source symlinks.sh
