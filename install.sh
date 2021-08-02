c5ba116c2a8b48c934a73c7dcd3e706bd0fd4873
CJ Dunteman <cjdunteman@gmail.com>
2021-08-02 16:43:51 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 os_name="$(uname -s)"
 
-if [ "$os_name" == "Darwin" ]; then
+if "$os_name" == "Darwin"
+then
     printf "Detected macOS."
     source mac/install.sh
-    
-elif [ "$os_name" == "Linux" ]; then
+elif "$os_name" == "Linux"
+then
     printf "Detected Ubuntu."
     source ubuntu/install.sh
 else
     printf "This script is only for macOS or Ubuntu."
-    fi
+fi
